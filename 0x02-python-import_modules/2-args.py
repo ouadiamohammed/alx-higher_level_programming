20690cd68b38e3689d3eec1b23c912740c79c368
mouadia <ouadia.mohammed@outlook.de>
2023-06-08 16:02:20 +0200

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         if argc == 2:
             print("1 argument:")
         else:
-            print("{} arguments:".format(argc -1))
+            print("{} arguments:".format(argc - 1))
         for i in range(argc - 1):
             print("{}: {}".format(i + 1, sys.argv[i + 1]))
