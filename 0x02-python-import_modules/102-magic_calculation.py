036595f648675e6d62e3087a9861f9f94a155d1e
mouadia <ouadia.mohammed@outlook.de>
2023-06-08 19:09:45 +0200

    update

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,7 +3,7 @@ def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(c, b)
-        for i in range(4, 6)
+        for i in range(4, 6):
             c = add(c, i)
         return c
     else:
