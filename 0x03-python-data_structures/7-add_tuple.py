76f831a22c37c4ce1522d49ff9b55a1de895b2e4
mouadia <ouadia.mohammed@outlook.de>
2023-06-11 11:19:36 +0200

    Tuples addition

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    res_tuple = ()
+    t1 = tuple_a + (0, 0)
+    t2 = tuple_b + (0, 0)
+    res_tuple = t1[0] + t2[0], t1[1] + t2[1]
+    return res_tuple
