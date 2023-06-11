6b03903871dd5e6c904a8d9c711194cdf67c6088
mouadia <ouadia.mohammed@outlook.de>
2023-06-11 12:02:17 +0200

    Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max = 0
+    if len(my_list) == 0:
+        return None
+    else:
+        for i in range(len(my_list)):
+            if max < my_list[i]:
+                max = my_list[i]
+    return max
