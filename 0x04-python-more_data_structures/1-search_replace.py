44e9183574b42999de543189f6bf4dcf9860a6d8
mouadia <ouadia.mohammed@outlook.de>
2023-06-14 18:17:01 +0200

    update

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 def search_replace(my_list, search, replace):
     new_list = []
     for i in my_list:
-        if i != replace:
+        if i != search:
             new_list.append(i)
         else:
             new_list.append(replace)
