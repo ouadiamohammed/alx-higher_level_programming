d8015342cf4701eba9ecd6a559c163a242fd0b5c
mouadia <ouadia.mohammed@outlook.de>
2023-06-26 21:22:04 +0200

    update

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,5 +6,5 @@ def safe_function(fct, *args):
         res = fct(*args)
         return res
     except Exception as error:
-        print("Exception:{}".format(error), file=sys.stderr)
+        print("Exception: {}".format(error), file=sys.stderr)
         return None
