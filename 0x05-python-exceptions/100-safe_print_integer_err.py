e38d627398a7dfb11167523d81b515625ef91fe3
mouadia <ouadia.mohammed@outlook.de>
2023-06-26 20:42:02 +0200

    update

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception as err:
-        sys.stderr.write("Exeption: {}\n".format(err))
+    except Exception as error:
+        sys.stderr.write("Exeption: {}\n".format(error))
         return False
