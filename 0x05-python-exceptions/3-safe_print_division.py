f8d654bd88fda5408ea4a27758443bce967d3777
mouadia <ouadia.mohammed@outlook.de>
2023-06-26 19:28:03 +0200

    Integers division with debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    res = a / b
+    try:
+        return (res)
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("{:d}".format(res))
