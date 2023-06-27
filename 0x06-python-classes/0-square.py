a64edd4ae8c9a57178c100ae2289bcee893ed9f6
mouadia <ouadia.mohammed@outlook.de>
2023-06-27 17:57:07 +0200

    ex00,  ex01

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    """Declaring a class that does nothing"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define new class square"""
+
+
+class Square:
+    """Square"""
+
+    def __init__(self, size):
+        """initializes attributes for square."""
+
+        self.__size = size
