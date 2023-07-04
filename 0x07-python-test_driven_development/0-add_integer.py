fcd605f7321e7a0650047314c6c3b1b8e339cbb0
mouadia <ouadia.mohammed@outlook.de>
2023-07-04 23:58:02 +0200

    update

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""add_integer method"""
+
+
+def add_integer(a, b=98):
+    """add to integer"""
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    
+    a = int(a)
+    b = int(b)
+    return a + b
