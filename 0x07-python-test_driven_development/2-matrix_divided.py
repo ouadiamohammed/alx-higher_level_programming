573cfeff1006db499bc91bdb67d9d1b37853c178
mouadia <ouadia.mohammed@outlook.de>
2023-09-01 21:59:47 +0200

    Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,22 @@
 #!usr/bin/python3
+"""function that divides all elements of a matrix"""
+
+def matrix_divided(matrix, div):
+    """Devides all elements of a matrix"""
+
+
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+
+
+    new_matrix = []
+    temp = []
+
+    for row in matrix:
+        for element in row:
+           result = round(element / div, 2)
+           temp.append(result)
+        new_matrix.append(temp)
+
+    return new_matrix
