289fe33c98cd8cdda750b2d2f4022542ffd2effd
mouadia <ouadia.mohammed@outlook.de>
2023-09-11 20:34:53 +0200

    my list

--- 0x0A-python-inheritance/1-main.py
+++ 0x0A-python-inheritance/1-main.py
@@ -2,10 +2,11 @@
 MyList = __import__('1-my_list').MyList
 
 my_list = MyList()
-my_list.append(1)
-my_list.append(20)
+my_list.append(5)
+my_list.append(80)
 my_list.append(4)
 my_list.append(0)
+my_list.append(10)
 
 print(my_list)
 my_list.print_sorted()
