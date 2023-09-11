20ddf83880faa6fa95bcd0b73f1d78b530846741
mouadia <ouadia.mohammed@outlook.de>
2023-09-11 23:07:12 +0200

    basegeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class BaseGeometry"""
+
+
+class BaseGeometry():
+    """class BaseGeometry"""
+    def area(self):
+        """method still not impelemented"""
+        return Exception("area() is not impelented")
