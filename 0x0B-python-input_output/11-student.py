1f93ecd2137d156c65136078cc03c8eee70e751a
mouadia <ouadia.mohammed@outlook.de>
2023-09-12 22:18:25 +0200

    exx 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Student class"""
+
+
+class Student:
+    """student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (type(attrs) == list and
+                all(type(element) == str for element in attrs)):
+            return {key: getattr(self, key)
+                    for key in attrs if hasattr(self, key)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        self.__dir__.update(json)
