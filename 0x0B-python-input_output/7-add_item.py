6e643c0ce07fa72e3c3c75f34ec0bfba3dc4954f
mouadia <ouadia.mohammed@outlook.de>
2023-09-12 23:15:26 +0200

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,7 +3,7 @@
 import json
 import sys
 
-save_tp_json_file = __import__('5-save_to_json_file').save_to_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 list = []
@@ -18,4 +18,4 @@ finally:
         if arg == sys.argv[0]:
             continue
         list.append(arg)
-    save_tp_json_file(list, filename)
+    save_to_json_file(list, filename)
