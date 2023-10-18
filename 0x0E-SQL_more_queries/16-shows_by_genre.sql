7707142214e91e2fa65f0b1479607a5e7ce5fa2f
mouadia <ouadia.mohammed@outlook.de>
2023-10-19 01:11:05 +0200

    update

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,2 +1,10 @@
--- lists all show and genres linked to that show
-SELECT title, name FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genres ON tv_show.genres.genre_id = tv_genres.id ORDER BY title ASC, name ASC;
+-- Lists all shows and genres linked to the show from the
+SELECT t.`title`, g.`name`
+  FROM `tv_shows` AS t
+       LEFT JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+
+       LEFT JOIN `tv_genres` AS g
+       ON s.`genre_id` = g.`id`
+ ORDER BY t.`title`, g.`name`;
+ 
