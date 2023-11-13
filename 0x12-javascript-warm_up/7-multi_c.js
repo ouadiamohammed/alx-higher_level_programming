aec4260fce5cf2f7ef6816db06516b4bc93cf6ff
mouadia <ouadia.mohammed@outlook.de>
2023-11-13 21:47:49 +0100

    js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const firstArgument = Number(process.argv[2]);
 
-if (Number.isNaN(firstArgument) || firstArgument < 1) {
+if (Number.isNaN(firstArgument)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < firstArgument; i++) {
