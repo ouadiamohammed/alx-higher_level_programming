7b0d770b03a08ddc066f47f49d56af4201547b05
mouadia <ouadia.mohammed@outlook.de>
2023-11-13 20:46:40 +0100

    js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const firstArgument = process.argv[2];
-const secondArgument = process.argv[2];
+const secondArgument = process.argv[3];
 
 console.log(`${firstArgument} is ${secondArgument}`);
