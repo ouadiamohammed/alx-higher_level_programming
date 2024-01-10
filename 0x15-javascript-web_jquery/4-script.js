591cf88270b5919a49b9c8a1dda4b8fd68ae3b8f
mouadia <ouadia.mohammed@outlook.de>
2024-01-10 19:56:31 +0100

    update

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    // Add a click event handler to the div with the id 'toggle_header'
+    $("#toggle_header").click(function () {
+      // Select the 'header' element and toggle the "red" and "green" classes
+      $("header").toggleClass("red green");
+    });
+  });
+  
---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    // Make an AJAX GET request to the URL
+    $.get("https://hellosalut.stefanbohacek.dev/?lang=fr", function (data) {
+      // Display the translation of "hello" in the div with the id 'hello'
+      $("#hello").text(data.hello);
+    });
+  });
+  
